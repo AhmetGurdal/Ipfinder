fc52a00c1158d848e8a88a52aab90682478d8d32
Azaraks-AG <ahmetgurdal32@gmail.com>
2017-03-18 07:23:25 -0400

    Message

--- ipfinder.py
+++ ipfinder.py
@@ -1,11 +1,42 @@
 import re
 from sys import argv
+import sys
 
-print argv
 
-if (argv)
-f = open("ip.txt","r")
+def writeAll():
+	print "----------------------"
+	print "Most probably IP addresses"
+	again = []
+	if len(excellent) > 0:
+		for  i in excellent:
+			if i not in again:
+				print '-> ' + i + ' is counted ' + str(excellent.count(i)) + " times."
+				again.append(i)
+	else:
+		print "Couldn't find a valid IP address."
+	again = []
+	if len(lookslike) > 0:
+		print "Less probably IP addresses"
+		for  i in lookslike:
+			if i not in again:
+				print "-> " + i + " is counted " + str(lookslike.count(i)) + " times."
 
+
+def help():
+	print "Help line"
+	print "-f \t-> For give file name"
+	print "-F \t-> For give file names file"
+	print "-l \t-> give specific ip address."
+	print "-L \t-> give specific ip addresses file."
+
+
+if ("-f" in argv):
+	fl = argv[argv.index("-f")+1]
+try:
+	f = open(fl,"r")
+except:
+	help()
+	sys.exit(1)
 best = re.findall(r'\d{1,4}\.\d{1,3}\.\d{1,3}\.\d{1,4}',f.read())
 
 excellent = []
@@ -26,19 +57,13 @@ for i in best:
 
 	else:
 		lookslike.append(i)
-print "----------------------"
-print "Most probably IP addresses"
-again = []
-if len(excellent) > 0:
-	for  i in excellent:
-		if i not in again:
-			print '-> ' + i + ' is counted ' + str(excellent.count(i)) + " times."
-			again.append(i)
-else:
-	print "Couldn't find a valid IP address."
-again = []
-if len(lookslike) > 0:
-	print "Less probably IP addresses"
-	for  i in lookslike:
-		if i not in again:
-			print "-> " + i + " is counted " + str(lookslike.count(i)) + " times."
+
+
+if ("-l" in argv):
+	
+	if (argv[argv.index("-l")+1] in excellent):
+		print argv[argv.index("-l")+1] + " is counted " + str(excellent.count(argv[argv.index("-l")+1])) + " times"
+	if (argv[argv.index("-l")+1] in lookslike):
+		print argv[argv.index("-l"+1)] + " is counted " + str(lookslike.count(argv[argv.index("-l")+1])) + " times"
+if ("-l" not in argv):
+	writeAll()
